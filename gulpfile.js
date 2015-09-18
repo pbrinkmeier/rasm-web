32b0e3cf05ae4cac6171ea26357dcafa4c03cecf
reg4in <gunzof@gmx.de>
2015-09-18 22:28:33 +0200

    add javascript to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
 'use strict'
 
+var browserify = require('gulp-browserify')
 var gulp = require('gulp')
 var stylus = require('gulp-stylus')
+var uglify = require('gulp-uglify')
 
 gulp.task('build-stylus', function () {
-  gulp
-  .src('./styles/rasm-web.styl')
-  .pipe(stylus({
-    compress: true
-  }))
+  gulp.src('./styles/rasm-web.styl')
+  .pipe(stylus({ compress: true }))
   .pipe(gulp.dest('./'))
 })
 
-gulp.task('default', [ 'build-stylus' ])
+gulp.task('build-javascript', function () {
+  gulp.src('./js/rasm-web.js')
+  .pipe(browserify())
+  .pipe(uglify())
+  .pipe(gulp.dest('./'))
+})
+
+gulp.task('default', [ 'build-stylus', 'build-javascript' ])
